728a9efc7c9b4485301f3726ff40943e1f205fb5
alisaleemh <ali.s1995@gmail.com>
2017-08-29 13:00:08 -0400

    fixed null style error on the marker click event

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 var Location = function (title, lng, lat  ){
   var self = this;
   this.title = title;
   this.location = {lat: lat, lng: lng};
 
-  this.infoWindow = new google.maps.InfoWindow();
+  this.infowindow = new google.maps.InfoWindow();
   this.streetViewService = new google.maps.StreetViewService() ;
   this.marker = new google.maps.Marker({
     position: new google.maps.LatLng(self.location.lng, self.location.lat),
@@ -13,82 +12,98 @@ var Location = function (title, lng, lat  ){
     animation: google.maps.Animation.DROP
   });
 
+  this.closeAllInfowindows = function() {
+    for (var i=0; i < viewModel.locations.length; i++) {
+      viewModel.locations[i].infowindow.close();
+    }
+  }
 
+  this.nullAllMarkers = function() {
+    for (var i=0; i < viewModel.locations.length; i++) {
+      viewModel.locations[i].marker.setMap(null);
+    }
+  }
 
   this.showInfoWindow = function() {
     for (var i=0; i < viewModel.locations.length; i++) {
-      viewModel.locations[i].infoWindow.close();
+      viewModel.locations[i].infowindow.close();
       viewModel.locations[i].marker.setMap(null);
     }
     self.streetViewService.getPanoramaByLocation(self.marker.position, 50, self.getStreetView);
 
     map.panTo(self.marker.getPosition());
-    // self.infoWindow.setContent(self.content);
+    self.infowindow.setContent(self.content);
     self.marker.setMap(map);
-    self.infoWindow.open(map, self.marker);
+    self.infowindow.open(map, self.marker);
   };
 
   this.getStreetView = function()  {
-    self.content = '<div>' + self.marker.title + '</div><div id="pano"></div>';
-    self.infoWindow.setContent('<div>' + self.title + '</div><div id="pano"></div>');
+    self.content = '<div>' + self.title + '</div><div id="pano"></div>';
+    self.infowindow.setContent(self.content);
 
-    var panoramaOptions = {
-      position: {
+    this.panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: {
           lat: self.location.lat,
           lng: self.location.lng
-      },      pov: {
-        heading: 160,
-        pitch: 0
+        }, pov: {
+          heading: 160,
+          pitch: 0
+        }
       }
-    };
-    var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('pano'), panoramaOptions);
-    };
-
-    this.addListener = google.maps.event.addListener(self.marker, 'click', (this.openInfowindow));
-  };
-
-  var viewModel = {
-
-    locations:[
-      new Location('Chinatown Homey Space', 40.7180628, -73.9961237),
-      new Location('TriBeCa Artsy Bachelor Pad', 40.7195264, -74.0089934),
-      new Location('East Village Hip Studio', 40.7281777, -73.984377),
-      new Location('Union Square Open Floor Plan', 40.7347062, -73.9895759),
-      new Location('Chelsea Loft', 40.7444883, -73.9949465)
-    ],
-    query: ko.observable(''),
-
-  };
-
-
-  // Search function for filtering through the list of locations based on the name of the location.
-  viewModel.search = ko.dependentObservable(function() {
-    var self = this;
-    console.log("search being called");
-    var search = this.query().toLowerCase();
-    return ko.utils.arrayFilter(self.locations, function(location) {
-      return location.title.toLowerCase().indexOf(search) >= 0;
-    });
-  }, viewModel);
-
-  function hideAllListings () {
-    for (var i=0; i < this.locations.length; i++){
-      viewModel.locations[i].marker.setMap(null);
-      console.log('hideAllListings being called');
-      console.log(this.locations[i].marker);
-    }
+    );
   }
 
-  function showAllListings() {
-    for (var i=0; i < this.locations.length; i++){
-      viewModel.locations[i].marker.setMap(map);
-      console.log('showAllListings being called');
-      console.log(this.locations[i].marker);
-    }
+  self.marker.addListener('click', function() {
+    self.closeAllInfowindows();
+    self.streetViewService.getPanoramaByLocation(self.marker.position, 50, self.getStreetView);
+    self.infowindow.setContent(self.content);
+    self.marker.setMap(map);
+    self.infowindow.open(map, self.marker);
+  });
+
+  // this.addListener = google.maps.event.addListener(self.marker, 'click', (this.openInfowindow));
+};
+
+var viewModel = {
+
+  locations:[
+    new Location('Chinatown Homey Space', 40.7180628, -73.9961237),
+    new Location('TriBeCa Artsy Bachelor Pad', 40.7195264, -74.0089934),
+    new Location('East Village Hip Studio', 40.7281777, -73.984377),
+    new Location('Union Square Open Floor Plan', 40.7347062, -73.9895759),
+    new Location('Chelsea Loft', 40.7444883, -73.9949465)
+  ],
+  query: ko.observable(''),
+
+};
+
+
+// Search function for filtering through the list of locations based on the name of the location.
+viewModel.search = ko.dependentObservable(function() {
+  var self = this;
+  var search = this.query().toLowerCase();
+  return ko.utils.arrayFilter(self.locations, function(location) {
+    if (location.title.toLowerCase().indexOf(search) < 0) {location.marker.setMap(null) };
+    if (location.title.toLowerCase().indexOf(search) >= 0) {location.marker.setMap(map) };
+    return location.title.toLowerCase().indexOf(search) >= 0;
+
+  });
+}, viewModel);
+
+function hideAllListings () {
+  for (var i=0; i < this.locations.length; i++){
+    viewModel.locations[i].marker.setMap(null);
   }
+}
+
+function showAllListings() {
+  for (var i=0; i < this.locations.length; i++){
+    viewModel.locations[i].marker.setMap(map);
+  }
+}
 
 
 
 
-  ko.applyBindings(viewModel);
+ko.applyBindings(viewModel);
