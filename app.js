bb75566204d3f910e8edd6d8665510c530eaee17
alisaleemh <ali.s1995@gmail.com>
2017-08-30 16:21:52 -0400

    added foursquare api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var Location = function (title, lng, lat  ){
+var Location = function (title, lng, lat, venueId ){
   var self = this;
   this.title = title;
   this.location = {lat: lat, lng: lng};
+  this.venueId = venueId;
 
   this.infowindow = new google.maps.InfoWindow();
   this.streetViewService = new google.maps.StreetViewService() ;
@@ -12,102 +13,132 @@ var Location = function (title, lng, lat  ){
     animation: google.maps.Animation.DROP
   });
 
-  this.closeAllInfowindows = function() {
-    for (var i=0; i < viewModel.locations.length; i++) {
-      viewModel.locations[i].infowindow.close();
-    }
-  }
+  this.getContent = function() {
+    var topTips = [];
+    var venueUrl = 'https://api.foursquare.com/v2/venues/' + self.venueId + '/tips?sort=recent&limit=3&v=20170509&client_id=GVXA4GBVK2BS3MBLNV5BHZAWP3HZINW0H1QK1XTNWH0ACQW5&client_secret=QAMIPBBSVDK4LXZA1KDCDK5Y0KWQJABV3D31ASXYJZFYB2YT';
 
-  this.nullAllMarkers = function() {
-    for (var i=0; i < viewModel.locations.length; i++) {
-      viewModel.locations[i].marker.setMap(null);
-    }
-  }
+    $.getJSON(venueUrl,
+      function(data) {
+        $.each(data.response.tips.items, function(i, tips){
+          // foursquare api has bug that doesn't limit the number of responses
+          if (i < 5) {topTips.push('<li>' + tips.text + '</li>') };
+        });
 
-  this.showInfoWindow = function() {
-    for (var i=0; i < viewModel.locations.length; i++) {
-      viewModel.locations[i].infowindow.close();
-      viewModel.locations[i].marker.setMap(null);
-    }
-    self.streetViewService.getPanoramaByLocation(self.marker.position, 50, self.getStreetView);
+      }).done(function(){
 
-    map.panTo(self.marker.getPosition());
-    self.infowindow.setContent(self.content);
-    self.marker.setMap(map);
-    self.infowindow.open(map, self.marker);
+        self.content = '<h2>' + self.title + '</h2>' + '<h3>5 Most Recent Comments</h3>' + '<ol class="tips">' + topTips.join('') + '</ol>';
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        self.content = '<h2>' + self.title + '</h2>' + '<h3>5 Most Recent Comments</h3>' + '<h4>Oops. There was a problem retrieving this location\'s comments.</h4>';
+        console.log('getJSON request failed! ' + textStatus);
+      });
+    }();
+
+    this.closeAllInfowindows = function() {
+      for (var i=0; i < viewModel.locations.length; i++) {
+        viewModel.locations[i].infowindow.close();
+      }
+    }
+
+    this.nullAllMarkers = function() {
+      for (var i=0; i < viewModel.locations.length; i++) {
+        viewModel.locations[i].marker.setMap(null);
+      }
+    }
+
+    this.showInfoWindow = function() {
+      for (var i=0; i < viewModel.locations.length; i++) {
+        viewModel.locations[i].infowindow.close();
+        viewModel.locations[i].marker.setMap(null);
+      }
+      self.streetViewService.getPanoramaByLocation(self.marker.position, 50, self.getStreetView);
+
+      map.panTo(self.marker.getPosition());
+      self.infowindow.setContent(self.content);
+      self.marker.setMap(map);
+      self.infowindow.open(map, self.marker);
+    };
+
+    this.getStreetView = function()  {
+      self.content = self.content.concat(' ' + '<div>' + '</div><div id="pano"></div>' );
+      console.log('getStreetView' + self.content);
+      self.infowindow.setContent(self.content);
+
+      this.panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), {
+          position: {
+            lat: self.location.lat,
+            lng: self.location.lng
+          }, pov: {
+            heading: 160,
+            pitch: 20
+          }
+        }
+      );
+    }
+
+    self.marker.addListener('click', function() {
+      self.closeAllInfowindows();
+      self.streetViewService.getPanoramaByLocation(self.marker.position, 50, self.getStreetView);
+      console.log(self.content);
+      self.infowindow.setContent(self.content);
+      self.marker.setMap(map);
+      self.infowindow.open(map, self.marker);
+    });
+
+    // this.addListener = google.maps.event.addListener(self.marker, 'click', (this.openInfowindow));
   };
 
-  this.getStreetView = function()  {
-    self.content = '<div>' + self.title + '</div><div id="pano"></div>';
-    self.infowindow.setContent(self.content);
-
-    this.panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('pano'), {
-        position: {
-          lat: self.location.lat,
-          lng: self.location.lng
-        }, pov: {
-          heading: 160,
-          pitch: 0
-        }
+  var model = {
+    locations: ko.observableArray([{
+      title: 'Park Ave Penthouse',
+      venueId: '52d884c5498ecf5c7cafe5ab',
+      location: {
+        lat: 40.7713024,
+        lng: -73.9632393
       }
-    );
-  }
-
-  self.marker.addListener('click', function() {
-    self.closeAllInfowindows();
-    self.streetViewService.getPanoramaByLocation(self.marker.position, 50, self.getStreetView);
-    self.infowindow.setContent(self.content);
-    self.marker.setMap(map);
-    self.infowindow.open(map, self.marker);
-  });
-
-  // this.addListener = google.maps.event.addListener(self.marker, 'click', (this.openInfowindow));
-};
-
-var model = {
-  locations: ko.observableArray([{
-          title: 'Park Ave Penthouse',
-          location: {
-              lat: 40.7713024,
-              lng: -73.9632393
-          }
-      },
-      {
-          title: 'Chelsea Loft',
-          location: {
-              lat: 40.7444883,
-              lng: -73.9949465
-          }
-      },
-      {
-          title: 'Union Square Open Floor Plan',
-          location: {
-              lat: 40.7347062,
-              lng: -73.9895759
-          }
-      },
-      {
-          title: 'East Village Hip Studio',
-          location: {
-              lat: 40.7281777,
-              lng: -73.984377
-          }
-      },
-      {
-          title: 'TriBeCa Artsy Bachelor Pad',
-          location: {
-              lat: 40.7195264,
-              lng: -74.0089934
-          }
-      },
-      {
-          title: 'Chinatown Homey Space',
-          location: {
-              lat: 40.7180628,
-              lng: -73.9961237
-          }
+    },
+    {
+      title: 'Chelsea Loft',
+      venueId: '52d884c5498ecf5c7cafe5ab',
+      location: {
+        lat: 40.7444883,
+        lng: -73.9949465
       }
+    },
+    {
+      title: 'Union Square Open Floor Plan',
+      venueId: '52d884c5498ecf5c7cafe5ab',
+      location: {
+        lat: 40.7347062,
+        lng: -73.9895759
+      }
+    },
+    {
+      title: 'East Village Hip Studio',
+      venueId: '52d884c5498ecf5c7cafe5ab',
+
+      location: {
+        lat: 40.7281777,
+        lng: -73.984377
+      }
+    },
+    {
+      title: 'TriBeCa Artsy Bachelor Pad',
+      venueId: '52d884c5498ecf5c7cafe5ab',
+      location: {
+        lat: 40.7195264,
+        lng: -74.0089934
+      },
+
+    },
+    {
+      title: 'Chinatown Homey Space',
+      venueId: '52d884c5498ecf5c7cafe5ab',
+      location: {
+        lat: 40.7180628,
+        lng: -73.9961237
+      }
+    }
   ]),
 }
 
@@ -120,9 +151,9 @@ var viewModel = {
 viewModel.instantiateLocations = function () {
   for (i=0;i<model.locations().length;i++)
   {
-    var location = new Location(model.locations()[i].title, model.locations()[i].location.lat, model.locations()[i].location.lng);
+    var location = new Location(model.locations()[i].title, model.locations()[i].location.lat, model.locations()[i].location.lng, model.locations()[i].venueId);
     viewModel.locations.push(location);
-    }
+  }
 }
 
 viewModel.instantiateLocations();
